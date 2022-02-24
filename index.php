e1f63be9bc7864b78d136c94dd13a75a3fcaab20
secret <o0pqds3900@gmail.com>
2022-02-24 20:54:11 +0800

    welly test

--- index.php
+++ index.php
@@ -4,8 +4,15 @@ include_once "test2.php";
 include_once "test3.php";
 
 #第1題
-$position = 8;//第幾個數字的值。
-echo fibonacci($position);
+echo fibonacci(0);
+echo "\n";
+echo fibonacci(1);
+echo "\n";
+echo fibonacci(2);
+echo "\n";
+echo fibonacci(3);
+echo "\n";
+echo fibonacci(4);
 echo "\n";
 #第2題
 echo test2();
